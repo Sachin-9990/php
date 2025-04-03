1a71a03d5b09aac3abff0801388ef1aa19f23738
Sachin-9990 <sachinwaghela073@gmail.com>
2025-04-03 19:38:26 +0530

    Create (60)strlower.php

---
+++ (60)strlower.php
@@ -0,0 +1,14 @@
+WAP in php to demonstrate the use of strlower()
+<br>
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+echo strtolower("Hello WORLD.");
+?>
+
+<br>
+"This Program is written by Sachin Waghela 0221BCA025"
+</body>
+</html>
