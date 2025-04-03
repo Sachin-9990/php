1f8e823df6377d417a9b9a4ffb1f81babf90f643
Sachin-9990 <sachinwaghela073@gmail.com>
2025-04-03 19:35:59 +0530

    Create (57)str word count.php

---
+++ (57)str word count.php
@@ -0,0 +1,14 @@
+WAP in php to demonstrate the use of str_word_count()
+<br>
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+echo str_word_count("Hello world!");
+?>
+
+<br>
+"This Program is written by Waghela 0221BCA025"
+</body>
+</html>
