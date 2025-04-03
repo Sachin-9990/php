4e0957d1e1675b82babe3631b902581edc814500
Sachin-9990 <sachinwaghela073@gmail.com>
2025-04-03 18:52:17 +0530

    Create (9)Resource type.php

---
+++ (9)Resource type.php
@@ -0,0 +1,9 @@
+WAP to demonstrate resource types in php
+<?php
+
+    // Open a file for reading
+    $handle = fopen("note.txt", "r");
+    var_dump($handle);
+    echo "<br>";
+    echo "This Program is written by Sachin Waghela 0221BCA025"
+?>
