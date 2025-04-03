0276d13f62f91a0a1f9bb6ef1b305edf63727ea9
Sachin-9990 <sachinwaghela073@gmail.com>
2025-04-03 19:37:21 +0530

    Create (59)strrev.php

---
+++ (59)strrev.php
@@ -0,0 +1,14 @@
+WAP in php to demonstrate the use of strrev()
+<br>
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+echo strrev("Hello world!");
+?>
+
+<br>
+"This Program is written by Sachin Waghela 0221BCA025"
+</body>
+</html>
