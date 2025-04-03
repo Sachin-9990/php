e71a0a4ccacf2683b293f8567c12af7eb3b2b1a2
Sachin-9990 <sachinwaghela073@gmail.com>
2025-04-03 19:36:38 +0530

    Create (58)str replace.php

---
+++ (58)str replace.php
@@ -0,0 +1,14 @@
+WAP in php to demonstrate the use of str_replace()
+<br>
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+echo str_replace("world", "Dolly", "Hello world!");
+?>
+
+<br>
+"This Program is written by Sachin Waghela 0221BCA025"
+</body>
+</html>
