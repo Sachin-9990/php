2af1353eecd7feca93172099bf2b2579cccd5b89
Sachin-9990 <sachinwaghela073@gmail.com>
2025-04-03 19:39:08 +0530

    Create (61)strupper.php

---
+++ (61)strupper.php
@@ -0,0 +1,14 @@
+WAP in php to demonstrate the use of strupper()
+<br>
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+echo strtoupper("Hello WORLD!");
+?>
+
+<br>
+"This Program is written by Sachin Waghela 0221BCA025"
+</body>
+</html>
