d72b6f300975b87558e7f5ef5c1e14ccc903fe90
Sachin-9990 <sachinwaghela073@gmail.com>
2025-04-03 19:40:04 +0530

    Create (62)ucfirst.php

---
+++ (62)ucfirst.php
@@ -0,0 +1,14 @@
+WAP in php to demonstrate the use of ucfirst()
+<br>
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+echo ucfirst("hello world!");
+?>
+
+<br>
+"This Program is written by Sachin Waghela 0221BCA025"
+</body>
+</html>
