b08b2eaadefbba285618eb078563b735a83f2515
Sachin-9990 <sachinwaghela073@gmail.com>
2025-04-03 19:34:26 +0530

    Create (55)Count number of words in string.php

---
+++ (55)Count number of words in string.php
@@ -0,0 +1,15 @@
+WAP in php to count number of words in a string 
+<br>
+<!DOCTYPE html>
+</html>
+</body>
+
+<?php
+echo str_word_count("Hello world!");
+?>
+
+<br>
+"This Program is written by Sachin Waghela 0221BCA025"
+</body>
+</html>
+
