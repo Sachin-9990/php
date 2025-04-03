4c0e4529b829526ed95e8731ff14f26a7d38d9f2
Sachin-9990 <sachinwaghela073@gmail.com>
2025-04-03 19:40:57 +0530

    Create (63)ucwords.php

---
+++ (63)ucwords.php
@@ -0,0 +1,14 @@
+WAP in php to demonstrate the use of ucwords()
+<br>
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+echo ucwords("hello world");
+?>
+
+<br>
+"This Program is written by Sachin Waghela 0221BCA025"
+</body>
+</html>
