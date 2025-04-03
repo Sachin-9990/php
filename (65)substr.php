5b112b26914e07c28a1c1c5cb2937fcbde8c2628
Sachin-9990 <sachinwaghela073@gmail.com>
2025-04-03 19:42:47 +0530

    Create (65)substr.php

---
+++ (65)substr.php
@@ -0,0 +1,14 @@
+WAP in php to demonstrate the use of substr()
+<br>
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+echo substr("Hello world",6);
+?>
+
+<br>
+"This Program is written by Sachin Waghela 0221BCA025"
+</body>
+</html>
